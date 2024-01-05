245fd1435bfda28d002f026c78edaa8e6dbd02cc
IvanK-QX <ivan.k@unitedtech.ai>
2024-01-05 17:53:20 +0200

    Add simple test

--- tests/e2e_test.spec.ts
+++ tests/e2e_test.spec.ts
@@ -2,16 +2,40 @@ import { test } from '@playwright/test';
 import { App } from '../pages/app';
 import { testData } from '../utils/dataset';
 
+// simple trainig flow 
+test('Registration flow', async ({page}) => {
+  const app = new App(page)
+  const email = testData.email
 
+  await page.goto('https://rahulshettyacademy.com/client')
+  await page.locator('[href="/client/auth/register"]').click()
+  await page.locator('#firstName').fill(testData.firstName)
+  await page.locator('#lastName').fill(testData.lastName)
+  await page.locator('#userEmail').fill(email)
+  await page.locator('#userMobile').fill(testData.phone)
+  await page.locator('#userPassword').fill(testData.pass)
+  await page.locator('#confirmPassword').fill(testData.pass)
+  await page.locator('[formcontrolname="gender"][value="Male"]').click()
+  await page.locator('[formcontrolname="occupation"]').selectOption('2: Student')
+  await page.locator('[type="checkbox"]').click()
+  await page.locator('[value="Register"]').click()
+  await page.locator('[routerlink="/auth"]').click()
+  await page.locator('#userEmail').fill(email)
+  await page.locator('#userPassword').fill(testData.pass)
+  await page.locator('#login').click()
+  await page.locator('#products').waitFor()
+})
+
+
+// e2e payment flow > PageObject
 test('e2e flow', async ({page}) => {
-  const app = new App(page),
-  email = testData.email
+  const app = new App(page)
 
   await app.loginPage.open()
   await app.loginPage.clickRegistrationButton()
   await app.singUpPage.inputFirstNameField(testData.firstName)
   await app.singUpPage.inputLastNameField(testData.lastName)
-  await app.singUpPage.inputEmailField(email)
+  const email = await app.singUpPage.inputEmailField(testData.email)
   await app.singUpPage.inputPhoneField(testData.phone)
   await app.singUpPage.inputPasswordField(testData.pass)
   await app.singUpPage.inputConfirmPasswordField(testData.pass)
